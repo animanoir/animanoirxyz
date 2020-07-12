79c3ea39a3369ae1379956a730f0899ffef4d9a7
Óscar A. Montiel <etherealflux94@gmail.com>
2020-07-12 00:52:23 -0500

    Cambios interesantes

---
+++ siteConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "Animanoir",
+  shortName: "ANoir",
+  description:
+    "Óscar A. Montiel Frontend Front-end Three.js Interacción Interactive Animation React JavaScript Web VR AR 3D ",
+  url: "https://animanoir.xyz", // Domain of your site without prefix!
+  prefix: "/",
+  author: "Óscar A. Montiel",
+  twitter: "@geosminu",
+}
---
+++ src/pages/about.js
@@ -0,0 +1,81 @@
+import React from "react"
+import { graphql, StaticQuery } from "gatsby"
+import Img from "gatsby-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import "../utils/normalize.css"
+import "../utils/css/screen.css"
+
+const AboutPage = ({ data }, location) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout title={siteTitle}>
+      <SEO title="About" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
+
+      <article className="post-content page-template no-image">
+        <div className="post-content-body">
+          <h2 id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-">
+            Desarrollador front-end
+          </h2>
+          <figure className="kg-card kg-image-card kg-width-full">
+            <Img
+              fluid={data.fotoAcerca.childImageSharp.fluid}
+              className="kg-image"
+            />
+            <figcaption>Óscar A. Montiel en su hábitat natural.</figcaption>
+          </figure>
+          <h3 id="dynamic-styles">Dynamic styles</h3>
+          <p>
+            London comes with photo-centric main layout best suited to
+            photography, graphics portfolios and other image-heavy uses.
+          </p>
+          <p>
+            Both post and page templates are light and minimal, with all the
+            focus on the content while the design of the theme gets out of the
+            way. Beneath the hood, London enjoys the full power of the{" "}
+            <a href="https://docs.ghost.org/api/handlebars-themes/">
+              Ghost Handlebars Theme API
+            </a>{" "}
+            to provide limitless customisation options and dynamic styles.
+          </p>
+          <p>
+            Don't forget to check out the{" "}
+            <a href="https://docs.ghost.org/integrations/">
+              Ghost Integrations Directory
+            </a>{" "}
+            for more ways to integrate Ghost with your favourite services.
+          </p>
+        </div>
+      </article>
+    </Layout>
+  )
+}
+
+const indexQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    fotoAcerca: file(relativePath: { eq: "acerca-de-oamm.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1360) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+export default props => (
+  <StaticQuery
+    query={indexQuery}
+    render={data => (
+      <AboutPage location={props.location} data={data} {...props} />
+    )}
+  />
+)
