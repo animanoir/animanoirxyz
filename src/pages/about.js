bef22d6022649ac6684f06409b984c10b70288fb
Óscar A. Montiel <etherealflux94@gmail.com>
2020-07-12 01:55:21 -0500

    Satisfactoriamente añade react-three-fiber.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useRef, useState } from "react"
 import { graphql, StaticQuery } from "gatsby"
 import Img from "gatsby-image"
 
@@ -7,6 +7,34 @@ import SEO from "../components/seo"
 
 import "../utils/normalize.css"
 import "../utils/css/screen.css"
+
+import { Canvas, useFrame } from "react-three-fiber"
+
+function Box(props) {
+  const mesh = useRef()
+
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={e => setActive(!active)}
+      onPointerOver={e => setHover(true)}
+      onPointerOut={e => setHover(false)}
+    >
+      <boxBufferGeometry attach="geometry" args={[4, 4, 4]} />
+      <meshStandardMaterial
+        attach="material"
+        color={hovered ? "hotpink" : "orange"}
+      />
+    </mesh>
+  )
+}
 
 const AboutPage = ({ data }, location) => {
   const siteTitle = data.site.siteMetadata.title
@@ -17,37 +45,32 @@ const AboutPage = ({ data }, location) => {
 
       <article className="post-content page-template no-image">
         <div className="post-content-body">
-          <h2 id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-">
-            Desarrollador front-end
-          </h2>
+          <div id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-">
+            <Canvas>
+              <Box position={[-1.2, 0, 0]} />
+            </Canvas>
+          </div>
           <figure className="kg-card kg-image-card kg-width-full">
             <Img
               fluid={data.fotoAcerca.childImageSharp.fluid}
               className="kg-image"
             />
-            <figcaption>Óscar A. Montiel en su hábitat natural.</figcaption>
           </figure>
-          <h3 id="dynamic-styles">Dynamic styles</h3>
-          <p>
-            London comes with photo-centric main layout best suited to
-            photography, graphics portfolios and other image-heavy uses.
-          </p>
-          <p>
-            Both post and page templates are light and minimal, with all the
-            focus on the content while the design of the theme gets out of the
-            way. Beneath the hood, London enjoys the full power of the{" "}
-            <a href="https://docs.ghost.org/api/handlebars-themes/">
-              Ghost Handlebars Theme API
-            </a>{" "}
-            to provide limitless customisation options and dynamic styles.
-          </p>
-          <p>
-            Don't forget to check out the{" "}
-            <a href="https://docs.ghost.org/integrations/">
-              Ghost Integrations Directory
-            </a>{" "}
-            for more ways to integrate Ghost with your favourite services.
-          </p>
+          <h3 id="dynamic-styles">Óscar A. Montiel</h3>
+          <h4>
+            <center>Front-end // Interacción // Animación // Multimedia</center>
+          </h4>
+          <h6>
+            <a
+              href="https://www.linkedin.com/in/oscaramontiel/"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+            <a href="https://ello.co/animanoir" target="_blank">
+              Ello
+            </a>
+          </h6>
         </div>
       </article>
     </Layout>
