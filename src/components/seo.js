56630cc0814e4c4c1d43f048732a424f6e21a852
Óscar A. Montiel <etherealflux94@gmail.com>
2020-08-06 19:51:48 -0500

    mejora de seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -42,7 +42,7 @@ function SEO({ description, lang, meta, keywords, title }) {
         },
         {
           property: `og:title`,
-          content: `Animanoir`,
+          content: title,
         },
         {
           property: `og:image`,
