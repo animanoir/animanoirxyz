6cd489707daded983d781340ff59deb83e9f557a
Óscar A. Montiel <etherealflux94@gmail.com>
2020-12-06 19:24:47 -0600

    fix thumbnail

--- src/templates/tag-page.js
+++ src/templates/tag-page.js
@@ -103,13 +103,6 @@ export const pageQuery = graphql`
             title
             description
             tags
-            thumbnail {
-              childImageSharp {
-                fluid(maxWidth: 1360) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
           }
         }
       }
